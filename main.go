c4b59fea566fe5faa0c64a2c9078e2478315c3d6
midorigreen <stitow14@gmail.com>
2018-10-11 14:56:36 +0900

    Add extract struct/struct comment

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"go/ast"
 	"go/parser"
 	"go/token"
 	"log"
+	"strings"
 )
 
 func run(dir string) error {
@@ -17,6 +19,34 @@ func run(dir string) error {
 	}
 	fmt.Println(pkgs)
 
+	m := map[string]string{}
+
+	for _, v := range pkgs {
+		for fk, fv := range v.Files {
+			if !strings.HasSuffix(fk, ".go") || strings.HasSuffix(fk, "_test.go") {
+				continue
+			}
+			commentMap := ast.NewCommentMap(fset, fv, fv.Comments)
+			for ck, _ := range commentMap {
+				switch tv := ck.(type) {
+				case *ast.GenDecl:
+					if tv.Tok != token.TYPE {
+						break
+					}
+					var typeName string
+					for _, sv := range tv.Specs {
+						ssv := sv.(*ast.TypeSpec)
+						typeName = ssv.Name.Name
+					}
+					m[typeName] = tv.Doc.Text()
+				default:
+					fmt.Println("skip")
+				}
+			}
+		}
+	}
+
+	fmt.Print(m)
 	return nil
 }
 
